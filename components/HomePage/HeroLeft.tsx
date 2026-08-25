34e41fefb25e92cf32df06242206eaf86d36d5e3
Muhammad Ali <alisdigitalwork@gmail.com>
2026-08-25 21:32:05 +0500

    Handle authentication anomalies linking with registered users and fixed relevant UI bugs for responsiveness

--- components/HomePage/HeroLeft.tsx
+++ components/HomePage/HeroLeft.tsx
@@ -22,7 +22,7 @@ export function HeroLeft() {
   return (
     <div id="hero-content" className="flex flex-col gap-6">
       {/* Heading */}
-      <h1 className="text-3xl sm:text-5xl font-semibold leading-tight tracking-tight text-text-700 min-h-28 md:min-h-32 ">
+      <h1 className="text-3xl sm:text-5xl font-semibold leading-tight tracking-tight text-text-700 min-h-22 md:min-h-28 ">
         {line1}
         {line2 && (
           <>
